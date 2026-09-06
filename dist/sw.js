e027e80556ebad0764271ef8c827222651c105c5
SORN7749 <stayhide69@gmail.com>
2026-09-06 23:13:49 +0700

    Add searchable knowledge article index

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mo-board-v1.1.0";
+const CACHE_NAME = "mo-board-v1.1.1";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
@@ -40,4 +40,3 @@ self.addEventListener("fetch", (event) => {
     })),
   );
 });
-
